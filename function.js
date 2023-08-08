f8b9a96b2079ad3b97ee858ad642790c96c8513e
muazzamgujjar <muazzamgujjarmuazzam@gmail.com>
2023-08-08 00:03:22 -0700

    second comment

---
+++ function.js
@@ -0,0 +1,61 @@
+// // function onePlusAvg(x,y){
+// //     return 1 +(x,y)/2
+// // }
+
+// // let  a = 1;
+// // let b = 2;
+// // let c = 3;
+//      console.log("one Plus Avg of a and b is ",onePlusAvg(a,b) )
+//      console.log("one Plus Avg of a and b is ", onePlusAvg(b,c))
+//      console.log("one Plus Avg of a and b is ", onePlusAvg(a,c))
+            //   &
+//     // this is an arrow function?
+//     let p = 9;
+//      let q = 7;
+//     const sum =() =>{
+//      return p+q  
+//     }
+//      console.log(p+q)
+            // &
+    // @ example tow
+
+    // // const hellow = () =>{
+    //     console.log( "Hey I am fine .")
+    //    return"hy"
+    // }
+    // // let v = hellow();
+    // // console.log(v)
+ 
+        //  CHAPTER # 3
+        // QUESTION # 1
+        // let marks={
+        //     ali : 23 ,
+        //     umer: 24,
+        //     uzair: 25,
+        //     junaid:26,
+        // }
+        // let i = "marks"
+// for(let  i=0; i<Object.keys(marks).length; i++ ){
+    // console.log("the marks of"+Object.keys(marks)[i]+"are"+marks[Object.keys(marks)[i]])
+console.log(i)
+// } 
+
+// QUESTION # 2
+// //  FOR IN LOOP
+// // for (let i in marks){
+//         console.log("the marks of"+[i]+ "are" + marks[i])
+// //  QUESTION # 3
+// //  WHILE LOOP?
+// let cn =23
+// let i
+// while(i !=cn){
+//     console.log("try again")
+//     i = prompt("Enter a number")
+// }
+
+    // // QUESTION # 4
+    // // TO FIND MEANS OF Number?
+    // const MEANS=(a,b,c,d) =>{
+    // return(a+b+c+d)/4
+    // }
+    // console.log(MEANS(4,5,6,7))
---
+++ index.js
@@ -0,0 +1,45 @@
+// // program to add first n natural numbers
+// let sum = 0
+
+// let n = prompt( 2)
+// n = Number.parseInt(n)
+
+// for ( let i=0; i<n; i++){
+//     sum+= (i+1)
+//     console.log((i+1),"+")
+// }
+// console.log("sum of first"+"n"+"natural number"+"sum")
+
+// for (let i=0; i<20; i++){
+//     console.log(i)
+// }
+// let obj = {
+//     ali:90,
+//     umer:80,
+//     uzair:50, 
+//     qamer:45,
+// }
+// // for in  loop
+// for (let a in obj){
+//     console.log( "marks of" + a + "are"   +  obj[a])
+// }
+// // for of  loop
+// for (let b in "ali"){
+//     console.log( b)
+// }
+//       while loops
+// let  n = prompt( "enter the value of n")
+// n= Number.parseInt(n)
+// let i=0;
+// while(i<5){
+//     console.log(i)
+//      i++;
+// }
+// // Do While loop 
+// let  n = prompt( "enter the value of n")
+// n= Number.parseInt(n)
+// let i = 10;
+// do{
+//     console.log(i)
+//       i++;
+// }while(i<=10)
